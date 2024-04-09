a13e360c4bea11335994a3e087d654ca59e75f69
Suryash Singh <f20220093@pilani.bits-pilani.ac.in>
2024-04-09 19:46:01 +0530

    admin menu updated

---
+++ AdminPage/src/com/entry_exit_system/model/StatusType.java
@@ -0,0 +1,5 @@
+package com.entry_exit_system.model;
+
+public enum StatusType {
+    PENDING, APPROVED, REJECT
+}
---
+++ HomePage/src/com/entry_exit_system/event/EventMenuSelected.java
@@ -0,0 +1,6 @@
+package com.entry_exit_system.event;
+
+public interface EventMenuSelected {
+
+    public void selected(int index);
+}
